6d7bcaa5964956c4ec8777674c80e4af00292501
hamam002 <em.hamamoto@yahoo.com>
2015-07-16 12:33:06 -0500

    Initial Commit. Calculator after tutorials plus square root button and power button.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,13 +13,81 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        display.text = "0"
     }
+    
+    var isInMiddleOfTyping = false
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var display: UILabel!
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if(isInMiddleOfTyping == true){
+            display.text = display.text! + "\(digit)"
+        }
+        
+        else{
+            display.text = digit
+            isInMiddleOfTyping = true
+        }
     }
-
-
+    
+    var operandStack = Array<Double>()
+    
+    var displayValue: Double{
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        
+        set{
+            display.text = "\(newValue)"
+            isInMiddleOfTyping = false
+        }
+    }
+    
+    @IBAction func enter() {
+        isInMiddleOfTyping = false
+        operandStack.append(displayValue)
+        println("Operand Stack = \(operandStack)")
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if(isInMiddleOfTyping){
+            enter()
+        }
+        
+        switch operation{
+        case "×":
+            performOperation {$0 * $1}
+        case "÷":
+            performOperation {$1 / $0}
+        case "+":
+            performOperation {$0 + $1}
+        case "−":
+            performOperation {$1 - $0}
+        case "√":
+            performOperation {sqrt($0)}
+        case "^":
+            performOperation {pow($1, $0)}
+        
+        default: break
+        }
+    }
+    
+    private func performOperation(operation: (Double, Double) -> Double){
+        if(operandStack.count >= 2){
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    private func performOperation(operation: Double -> Double){
+        if(operandStack.count >= 1){
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
 }
 
